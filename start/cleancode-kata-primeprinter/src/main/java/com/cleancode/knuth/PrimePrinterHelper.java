12ac61615f5cafbc28cda21caca4b2f352290a17
AlpinooO <alpino.cdc@gmail.com>
2024-12-15 23:01:23 +0100

    commit 7

--- start/cleancode-kata-primeprinter/src/main/java/com/cleancode/knuth/PrimePrinterHelper.java
+++ start/cleancode-kata-primeprinter/src/main/java/com/cleancode/knuth/PrimePrinterHelper.java
@@ -11,11 +11,6 @@ public class PrimePrinterHelper {
     private int square;
     private int n;
     private int[] multiples;
-    private int pageNumber;
-    private int pageOffset;
-    private int rowOffset;
-    private int column;
-    private int linesPerPage;
 
     public PrimePrinterHelper(int numberOfNumbers) {
         this.numberOfNumbers = numberOfNumbers;
@@ -59,28 +54,5 @@ public class PrimePrinterHelper {
 
     public int[] getPrimes() {
         return primes;
-    }
-
-    public void printPrimes(int[] numbers, int numberOfNumbers) {
-        int pagenumber = 1;
-        int pageoffset = 1;
-        while (pageoffset <= numberOfNumbers) {
-            System.out.print("The First ");
-            System.out.print(Integer.toString(numberOfNumbers));
-            System.out.print(" Prime Numbers === Page ");
-            System.out.print(Integer.toString(pagenumber));
-            System.out.println("\n");
-            for (int rowoffset = pageoffset; rowoffset <= pageoffset + linesPerPage - 1; rowoffset++) {
-                for (int column = 0; column <= this.column - 1; column++) {
-                    if (rowoffset + column * linesPerPage <= numberOfNumbers) {
-                        System.out.printf("%10d", numbers[rowoffset + column * linesPerPage]);
-                    }
-                }
-                System.out.println();
-            }
-            System.out.println("\f");
-            pagenumber++;
-            pageoffset += linesPerPage * this.column;
-        }
     }
 }
