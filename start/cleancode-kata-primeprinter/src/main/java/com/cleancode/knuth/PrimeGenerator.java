4bf92e390d029c2273dd61763cc2b722b5e19d11
AlpinooO <alpino.cdc@gmail.com>
2024-12-16 00:00:35 +0100

    commit 9

--- start/cleancode-kata-primeprinter/src/main/java/com/cleancode/knuth/PrimeGenerator.java
+++ start/cleancode-kata-primeprinter/src/main/java/com/cleancode/knuth/PrimeGenerator.java
@@ -1,47 +1,52 @@
 package com.cleancode.knuth;
 
 public class PrimeGenerator {
-    private final int M;
+    private final int ordmax = 30;
     private int[] primes;
+    private int candidate;
+    private int primeIndex;
+    private boolean possiblyPrime;
+    private int ord;
+    private int square;
+    private int n;
+    private int[] multiples;
 
-    public PrimeGenerator(int M) {
-        this.M = M;
-        this.primes = new int[M + 1];
-        generatePrimes();
+    public PrimeGenerator(int numberOfPrimes) {
+        this.primes = new int[numberOfPrimes + 1];
+        this.multiples = new int[ordmax + 1];
+        generatePrimes(numberOfPrimes);
     }
 
-    private void generatePrimes() {
-        final int ORDMAX = 30;
-        int K = 1;
-        int J = 1;
-        int ORD = 2;
-        int SQUARE = 9;
-        int N = 0;
-        int[] MULT = new int[ORDMAX + 1];
+    private void generatePrimes(int numberOfPrimes) {
+        primeIndex = 1;
+        candidate = 1;
+        ord = 2;
+        square = 9;
+        n = 0;
 
         primes[1] = 2;
 
-        while (K < M) {
+        while (primeIndex < numberOfPrimes) {
             boolean JPRIME;
             do {
-                J += 2;
-                if (J == SQUARE) {
-                    ORD++;
-                    SQUARE = primes[ORD] * primes[ORD];
-                    MULT[ORD - 1] = J;
+                candidate += 2;
+                if (candidate == square) {
+                    ord++;
+                    square = primes[ord] * primes[ord];
+                    multiples[ord - 1] = candidate;
                 }
-                N = 2;
+                n = 2;
                 JPRIME = true;
-                while (N < ORD && JPRIME) {
-                    while (MULT[N] < J)
-                        MULT[N] += primes[N] + primes[N];
-                    if (MULT[N] == J)
+                while (n < ord && JPRIME) {
+                    while (multiples[n] < candidate)
+                        multiples[n] += primes[n] + primes[n];
+                    if (multiples[n] == candidate)
                         JPRIME = false;
-                    N++;
+                    n++;
                 }
             } while (!JPRIME);
-            K++;
-            primes[K] = J;
+            primeIndex++;
+            primes[primeIndex] = candidate;
         }
     }
 
--- start/cleancode-kata-primeprinter/src/main/java/com/cleancode/knuth/PrimePrinter.java
+++ start/cleancode-kata-primeprinter/src/main/java/com/cleancode/knuth/PrimePrinter.java
@@ -1,15 +1,15 @@
 package com.cleancode.knuth;
 
 public class PrimePrinter {
-    private static final int NUMBER_OF_NUMBERS = 1000;
+    private static final int NUMBER_OF_PRIMES = 1000;
     private static final int LINES_PER_PAGE = 50;
     private static final int COLUMN_COUNT = 4;
 
     public static void main(String[] args) {
-        PrimeGenerator primeGenerator = new PrimeGenerator();
+        PrimeGenerator primeGenerator = new PrimeGenerator(NUMBER_OF_PRIMES);
         NumberPrinter numberPrinter = new NumberPrinter(LINES_PER_PAGE, COLUMN_COUNT);
 
-        int[] primes = primeGenerator.generatePrimes(NUMBER_OF_NUMBERS);
-        numberPrinter.print(primes, NUMBER_OF_NUMBERS); // Adjusted parameters
+        int[] primes = primeGenerator.getPrimes();
+        numberPrinter.print(primes, NUMBER_OF_PRIMES);
     }
 }
